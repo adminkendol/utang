adde21617785a2d2dd558232da98efea7410bd78
adminkendol <chandra.comment@gmail.com>
2017-12-31 12:44:23 +0700

    First repo

---
+++ app/src/main/java/com/source/utang/MainActivity.java
@@ -0,0 +1,159 @@
+package com.source.utang;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Arrays;
+import nl.psdcompany.duonavigationdrawer.views.DuoDrawerLayout;
+import nl.psdcompany.duonavigationdrawer.views.DuoMenuView;
+import nl.psdcompany.duonavigationdrawer.widgets.DuoDrawerToggle;
+
+public class MainActivity extends AppCompatActivity implements DuoMenuView.OnMenuClickListener {
+    private MenuAdapter mMenuAdapter;
+    private ViewHolder mViewHolder;
+
+    private ArrayList<String> mTitles = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTitles = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.menuOptions)));
+        // Initialize the views
+        mViewHolder = new ViewHolder();
+        // Handle toolbar actions
+        handleToolbar();
+        // Handle menu actions
+        handleMenu();
+        // Handle drawer actions
+        handleDrawer();
+        // Show main fragment in container
+        goToFragment(new MainFragment2(), false);
+        mMenuAdapter.setViewSelected(0, true);
+        setTitle(mTitles.get(0));
+
+    }
+
+    private void handleToolbar() {
+        setSupportActionBar(mViewHolder.mToolbar);
+    }
+
+    private void handleDrawer() {
+        DuoDrawerToggle duoDrawerToggle = new DuoDrawerToggle(this,
+                mViewHolder.mDuoDrawerLayout,
+                mViewHolder.mToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        mViewHolder.mDuoDrawerLayout.setDrawerListener(duoDrawerToggle);
+        duoDrawerToggle.syncState();
+    }
+
+    private void handleMenu() {
+        mMenuAdapter = new MenuAdapter(mTitles);
+        //Log.d("menu", "new page");
+        mViewHolder.mDuoMenuView.setOnMenuClickListener(this);
+        mViewHolder.mDuoMenuView.setAdapter(mMenuAdapter);
+    }
+
+    @Override
+    public void onFooterClicked() {
+        //Toast.makeText(this, "onFooterClicked", Toast.LENGTH_SHORT).show();
+        exit();
+    }
+
+    @Override
+    public void onHeaderClicked() {
+        Toast.makeText(this, "onHeaderClicked", Toast.LENGTH_SHORT).show();
+    }
+
+    private void goToFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        //transaction.add(R.id.container, fragment).commit();
+        //transaction.remove(fragment);
+        transaction.replace(R.id.container, fragment);
+        //transaction.addToBackStack(null);
+        // Commit the transaction
+        transaction.commit();
+    }
+
+    @Override
+    public void onOptionClicked(int position, Object objectClicked) {
+        // Set the toolbar title
+        setTitle(mTitles.get(position));
+        Log.d("menu", String.valueOf(position));
+        // Set the right options selected
+        mMenuAdapter.setViewSelected(position, true);
+        // Navigate to the right fragment
+        switch (position) {
+            default:
+                goToFragment(new MainFragment2(), false);
+                break;
+            case 2:
+                goToFragment(new MainFragment(), false);
+                break;
+        }
+        // Close the drawer
+        mViewHolder.mDuoDrawerLayout.closeDrawer();
+    }
+
+    private class ViewHolder {
+        private DuoDrawerLayout mDuoDrawerLayout;
+        private DuoMenuView mDuoMenuView;
+        private Toolbar mToolbar;
+        ViewHolder() {
+            mDuoDrawerLayout = (DuoDrawerLayout) findViewById(R.id.drawer);
+            mDuoMenuView = (DuoMenuView) mDuoDrawerLayout.getMenuView();
+            mToolbar = (Toolbar) findViewById(R.id.toolbar);
+            //mToolbar.setBackgroundColor((Color.parseColor("#80000000")));
+        }
+    }
+
+    public void exit() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Exit ?")
+                .setCancelable(false)
+                .setPositiveButton("Ya", new
+                        DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //DrawerActivity.this.finish();
+                                /*Intent intent;
+                                intent = new Intent(DrawerActivity.this, LoginActivity.class);
+                                startActivity(intent);*/
+                                android.os.Process.killProcess(android.os.Process.myPid());
+                            }
+                        })
+                .setNegativeButton("Tidak", new
+                        DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int arg1) {
+                                // TODO Auto-generated method stub
+                                dialog.cancel();
+                            }
+
+                        }).show();
+    }
+    int backButtonCount = 0;
+    public void onBackPressed(){
+        if(backButtonCount >= 1){
+            //moveTaskToBack(true);
+            //finish();
+            //android.os.Process.killProcess(android.os.Process.myPid());
+            exit();
+        }else{
+            Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
+            backButtonCount++;
+        }
+    }
+}
